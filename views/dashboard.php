77d3afc3f041f875069ca0e8e1e701c1bc28b183
Pi0-dev <thanhvpt11235@gmail.com>
2020-10-06 05:46:39 +1100

    Test

--- views/dashboard.php
+++ views/dashboard.php
@@ -18,19 +18,64 @@ if(null == $username || null == $permission){
   <!--head-->
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Ezychef Dashboard</title>
+    <link rel="stylesheet" href="/views/css/dashboard.css">
   </head>
 
   <!--body-->
-  <body>
-    <h1>This is the dashboard</h1>
-    <form class="" action="/test/motherfucker" method="post">
-      <input type="submit" name="Fuck" value="fuck">
-    </form>
+  <body class="body">
+
+    <!--header-->
+    <div class="header">
+      <h1>This is the header</h1>
+    </div>
+
+    <!--horizontal navigation-->
+    <div class="navi">
+      <h1>Navigation bar fuck</h1>
+    </div>
+
+    <!--body container-->
+    <div class="body-container">
+
+      <!--vertical navigation-->
+      <div class="menu">
+        <ul class="menu-list">
+          <li> <a href="#">Menu 1</a> </li>
+          <li> <a href="#">Menu 1</a> </li>
+          <li> <a href="#">Menu 1</a> </li>
+          <li> <a href="#">Menu 1</a> </li>
+          <li> <a href="#">Menu 1</a> </li>
+          <li> <a href="#">Menu 1</a> </li>
+        </ul>
+      </div>
+
+      <!--main-->
+      <div class="main">
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+        <h1>Main</h1><br>
+      </div>
+
+    </div>
+
+    <!--footer-->
+    <div class="footer">
+      <h1>Ezychef footer</h1>
+    </div>
+
+
   </body>
 
 </html>
-
-<?php echo 'Yeah, this is the fucking dashboard'; ?>
-
-<?php echo $_SESSION['USER']; echo $_SESSION['PERMISSION']; ?>
