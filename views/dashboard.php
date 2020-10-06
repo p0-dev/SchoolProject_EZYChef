80c1cb3e4689c6ed55c299710bc88717cce68730
Pi0-dev <thanhvpt11235@gmail.com>
2020-10-07 00:16:38 +1100

    Dashboard website design

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,95 +1,124 @@
+<!--This master page will be root page of all page!-->
+
+<!--CHECKING SESSION AND DIRECT ACCESS FROM USERS-->
 <?php
-session_start();
-//checking direct access from users
-if(!defined('AccessAllowance')){
-  exit('Something went wrong! Life sucks, hah!');
-}
-//checking user session for double security
+//check session
 $username = $_SESSION['USER'];
 $permission = $_SESSION['PERMISSION'];
 if(null == $username || null == $permission){
   exit('Something went wrong! Life sucks, hah!');
 }
+//check direct access
+if(!defined('AccessAllowance')){
+  exit('Something went wrong! Life sucks, hah!');
+}
 ?>
+
+<!--TEST-->
+<?php
+//remove session for testing
+session_destroy();
+ ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-
-  <!--head-->
   <head>
     <meta charset="utf-8">
-    <title>Ezychef Dashboard</title>
-    <style media="screen">
-    </style>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test site</title>
     <link rel="stylesheet" href="/views/css/dboard.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="viewport" content="height=device-height, initial-scale=1.0">
   </head>
-
-  <!--body-->
-  <body class="body">
-
+  <body>
     <!--header-->
     <div class="header">
-      <h1>Ezychef Header</h1>
+      <h1>EZYCHEF</h1><br>
+      <h3>What the fuck this is, it sucks!</h3>
     </div>
 
-    <!--horizontal navigation-->
+    <!--navi-->
     <div class="navi">
-      <div class="menu-hori">
-        <ul class="menu-hori">
-          <li id="list"> <a id="menu" class="active" href="#">Dashboard</a> </li>
-          <li id="list"> <a id="menu" class="" href="#">Dashboard</a> </li>
-          <li id="list"> <a id="menu" class="" href="#">Dashboard</a> </li>
-          <li id="list"> <a id="menu" class="" href="#">Dashboard</a> </li>
-          <li id="list"> <a id="menu" class="" href="#">Dashboard</a> </li>
-          <li id="list"> <a id="menu" class="" href="#">Dashboard</a> </li>
-        </ul>
-      </div>
+      <ul>
+        <li class=""> <a href="#" class="active">HOME</a> </li>
+        <li> <a href="#">HOME</a> </li>
+        <li> <a href="#">HOME</a> </li>
+        <li> <a href="#">HOME</a> </li>
+        <li> <a href="#">HOME</a> </li>
+        <li> <a href="#">HOME</a> </li>
+      </ul>
     </div>
 
     <!--body container-->
     <div class="body-container">
 
-      <!--vertical navigation-->
-      <div class="menu">
-        <ul class="menu-list">
-          <p class="submenu">Actions</p>
-          <li id=""> <a id="submenu" class="" href="#">Dashboard</a> </li>
-          <li id=""> <a id="submenu" class="" href="#">Dashboard</a> </li>
-          <li id=""> <a id="submenu" class="" href="#">Dashboard</a> </li>
-          <li id=""> <a id="submenu" class="" href="#">Dashboard</a> </li>
-          <li id=""> <a id="submenu" class="" href="#">Dashboard</a> </li>
-          <li id=""> <a id="submenu" class="" href="#">Dashboard</a> </li>
+      <!--navigation-->
+      <div class="navigation">
+        <ul class="">
+          <p>SUB-MENU</p>
+          <li> <a href="#" class="active">HOME</a> </li>
+          <li> <a href="#">HOME</a> </li>
+          <li> <a href="#">HOME</a> </li>
+          <li> <a href="#">HOME</a> </li>
+          <li> <a href="#">HOME</a> </li>
+          <li> <a href="#">HOME</a> </li>
         </ul>
       </div>
 
       <!--main-->
       <div class="main">
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
-        <h1>Main</h1><br>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
+        <h1>fuck</h1>
       </div>
 
     </div>
 
     <!--footer-->
+    <br>
+    <hr>
     <div class="footer">
-      <h1>Ezychef footer</h1>
+      <h1>Ezychef information:</h1>
+      <h3>This is a fucking company with a fucking CEO!</h3>
     </div>
 
-
+    <!---->
+    <!---->
+    <!---->
   </body>
-
 </html>
